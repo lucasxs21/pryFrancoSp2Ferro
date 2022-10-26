0e85fa829c897966a96715e51cebbf316ca48f87
lucasxs21 <lucasfranco147@gmail.com>
2022-10-26 11:47:09 -0300

    Agregar archivos de proyecto.

---
+++ pryFrancoSp2Ferro/frmMain.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace pryFrancoSp2Ferro
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblDias = new System.Windows.Forms.Label();
+            this.lblDistancia = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.mrcCosto = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(276, 132);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // lblDias
+            // 
+            this.lblDias.AutoSize = true;
+            this.lblDias.Location = new System.Drawing.Point(111, 57);
+            this.lblDias.Name = "lblDias";
+            this.lblDias.Size = new System.Drawing.Size(35, 16);
+            this.lblDias.TabIndex = 3;
+            this.lblDias.Text = "Dias";
+            // 
+            // lblDistancia
+            // 
+            this.lblDistancia.AutoSize = true;
+            this.lblDistancia.Location = new System.Drawing.Point(111, 132);
+            this.lblDistancia.Name = "lblDistancia";
+            this.lblDistancia.Size = new System.Drawing.Size(63, 16);
+            this.lblDistancia.TabIndex = 4;
+            this.lblDistancia.Text = "Distancia";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(276, 42);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // mrcCosto
+            // 
+            this.mrcCosto.Location = new System.Drawing.Point(114, 224);
+            this.mrcCosto.Name = "mrcCosto";
+            this.mrcCosto.Size = new System.Drawing.Size(399, 141);
+            this.mrcCosto.TabIndex = 6;
+            this.mrcCosto.TabStop = false;
+            this.mrcCosto.Text = "costo";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 450);
+            this.Controls.Add(this.mrcCosto);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.lblDistancia);
+            this.Controls.Add(this.lblDias);
+            this.Controls.Add(this.textBox2);
+            this.Name = "frmMain";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblDias;
+        private System.Windows.Forms.Label lblDistancia;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.GroupBox mrcCosto;
+    }
+}
+

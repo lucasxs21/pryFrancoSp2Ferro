4c73f192d359ccbc3b74eb885342d55b2f426908
franco <Alumnos@PC61>
2022-10-28 13:10:16 -0300

    no se cierra

--- pryFrancoSp2Ferro/frmMain.Designer.cs
+++ pryFrancoSp2Ferro/frmMain.Designer.cs
@@ -36,7 +36,7 @@
             this.mrcCosto = new System.Windows.Forms.GroupBox();
             this.txtTotal = new System.Windows.Forms.TextBox();
             this.txtPrecio = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
             this.lblPrecio = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numUpDown)).BeginInit();
             this.mrcCosto.SuspendLayout();
@@ -44,33 +44,38 @@
             // 
             // txtDistancia
             // 
-            this.txtDistancia.Location = new System.Drawing.Point(276, 132);
+            this.txtDistancia.Location = new System.Drawing.Point(207, 107);
+            this.txtDistancia.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDistancia.MaxLength = 5;
             this.txtDistancia.Name = "txtDistancia";
-            this.txtDistancia.Size = new System.Drawing.Size(120, 22);
+            this.txtDistancia.Size = new System.Drawing.Size(91, 20);
             this.txtDistancia.TabIndex = 1;
             this.txtDistancia.TextChanged += new System.EventHandler(this.txtDistancia_TextChanged);
             // 
             // lblDias
             // 
             this.lblDias.AutoSize = true;
-            this.lblDias.Location = new System.Drawing.Point(111, 57);
+            this.lblDias.Location = new System.Drawing.Point(83, 46);
+            this.lblDias.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblDias.Name = "lblDias";
-            this.lblDias.Size = new System.Drawing.Size(35, 16);
+            this.lblDias.Size = new System.Drawing.Size(28, 13);
             this.lblDias.TabIndex = 3;
             this.lblDias.Text = "Dias";
             // 
             // lblDistancia
             // 
             this.lblDistancia.AutoSize = true;
-            this.lblDistancia.Location = new System.Drawing.Point(111, 132);
+            this.lblDistancia.Location = new System.Drawing.Point(83, 107);
+            this.lblDistancia.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblDistancia.Name = "lblDistancia";
-            this.lblDistancia.Size = new System.Drawing.Size(63, 16);
+            this.lblDistancia.Size = new System.Drawing.Size(51, 13);
             this.lblDistancia.TabIndex = 4;
             this.lblDistancia.Text = "Distancia";
             // 
             // numUpDown
             // 
-            this.numUpDown.Location = new System.Drawing.Point(276, 55);
+            this.numUpDown.Location = new System.Drawing.Point(207, 45);
+            this.numUpDown.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.numUpDown.Maximum = new decimal(new int[] {
             30,
             0,
@@ -82,7 +87,7 @@
             0,
             0});
             this.numUpDown.Name = "numUpDown";
-            this.numUpDown.Size = new System.Drawing.Size(120, 22);
+            this.numUpDown.Size = new System.Drawing.Size(90, 20);
             this.numUpDown.TabIndex = 5;
             this.numUpDown.Value = new decimal(new int[] {
             1,
@@ -94,11 +99,13 @@
             // 
             this.mrcCosto.Controls.Add(this.txtTotal);
             this.mrcCosto.Controls.Add(this.txtPrecio);
-            this.mrcCosto.Controls.Add(this.label2);
+            this.mrcCosto.Controls.Add(this.lblTotal);
             this.mrcCosto.Controls.Add(this.lblPrecio);
-            this.mrcCosto.Location = new System.Drawing.Point(114, 224);
+            this.mrcCosto.Location = new System.Drawing.Point(86, 182);
+            this.mrcCosto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.mrcCosto.Name = "mrcCosto";
-            this.mrcCosto.Size = new System.Drawing.Size(304, 141);
+            this.mrcCosto.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.mrcCosto.Size = new System.Drawing.Size(228, 115);
             this.mrcCosto.TabIndex = 6;
             this.mrcCosto.TabStop = false;
             this.mrcCosto.Text = "costo";
@@ -106,49 +113,55 @@
             // txtTotal
             // 
             this.txtTotal.Enabled = false;
-            this.txtTotal.Location = new System.Drawing.Point(162, 90);
+            this.txtTotal.Location = new System.Drawing.Point(122, 73);
+            this.txtTotal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtTotal.Name = "txtTotal";
-            this.txtTotal.Size = new System.Drawing.Size(120, 22);
+            this.txtTotal.Size = new System.Drawing.Size(91, 20);
             this.txtTotal.TabIndex = 3;
             // 
             // txtPrecio
             // 
             this.txtPrecio.Enabled = false;
-            this.txtPrecio.Location = new System.Drawing.Point(162, 28);
+            this.txtPrecio.Location = new System.Drawing.Point(122, 23);
+            this.txtPrecio.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPrecio.Name = "txtPrecio";
-            this.txtPrecio.Size = new System.Drawing.Size(120, 22);
+            this.txtPrecio.Size = new System.Drawing.Size(91, 20);
             this.txtPrecio.TabIndex = 2;
             this.txtPrecio.Text = "5$";
             // 
-            // label2
+            // lblTotal
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(68, 90);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Total";
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(51, 73);
+            this.lblTotal.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(31, 13);
+            this.lblTotal.TabIndex = 1;
+            this.lblTotal.Text = "Total";
+            this.lblTotal.Click += new System.EventHandler(this.label2_Click);
             // 
             // lblPrecio
             // 
             this.lblPrecio.AutoSize = true;
-            this.lblPrecio.Location = new System.Drawing.Point(38, 31);
+            this.lblPrecio.Location = new System.Drawing.Point(28, 25);
+            this.lblPrecio.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPrecio.Name = "lblPrecio";
-            this.lblPrecio.Size = new System.Drawing.Size(68, 16);
+            this.lblPrecio.Size = new System.Drawing.Size(55, 13);
             this.lblPrecio.TabIndex = 0;
             this.lblPrecio.Text = "Precio Km";
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(493, 450);
+            this.ClientSize = new System.Drawing.Size(370, 366);
             this.Controls.Add(this.mrcCosto);
             this.Controls.Add(this.numUpDown);
             this.Controls.Add(this.lblDistancia);
             this.Controls.Add(this.lblDias);
             this.Controls.Add(this.txtDistancia);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmMain";
@@ -170,7 +183,7 @@
         private System.Windows.Forms.GroupBox mrcCosto;
         private System.Windows.Forms.TextBox txtTotal;
         private System.Windows.Forms.TextBox txtPrecio;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTotal;
         private System.Windows.Forms.Label lblPrecio;
     }
 }
--- pryFrancoSp2Ferro/frmMain.cs
+++ pryFrancoSp2Ferro/frmMain.cs
@@ -24,24 +24,34 @@ namespace pryFrancoSp2Ferro
 
         private void txtDistancia_TextChanged(object sender, EventArgs e)
         {
-           
-            if (numUpDown.Value >= 1)
+           string ComprueboDistancia = txtDistancia.Text.Trim();
+            if (ComprueboDistancia.Length > 0)
             {
-                if (Convert.ToInt32(txtDistancia.Text)>= 100 && numUpDown.Value >= 7)
+                int distancia = Convert.ToInt32(txtDistancia.Text);
+                if (distancia > 0)
                 {
-                    resultado = Convert.ToInt32(txtDistancia.Text) * 5;
-                    txtTotal.Text = Convert.ToString(resultado);
-                    MessageBox.Show("Tiene descuento del 50%");
+                    if (Convert.ToInt32(txtDistancia.Text) >= 100 && numUpDown.Value >= 7)
+                    {
+                        resultado = Convert.ToInt32(txtDistancia.Text) * 5;
+                        txtTotal.Text = Convert.ToString(resultado);
+                        MessageBox.Show("Tiene descuento del 50%");
 
-                }
-                else
-                {
-                    resultado = Convert.ToInt32(txtDistancia.Text) * 5;
-                    resultado = resultado * 2;
-                    txtTotal.Text = Convert.ToString(resultado) ;
+                    }
+                    else
+                    {
+                        resultado = Convert.ToInt32(txtDistancia.Text) * 5;
+                        resultado = resultado * 2;
+                        txtTotal.Text = Convert.ToString(resultado);
 
+                    }
                 }
+                
             }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
